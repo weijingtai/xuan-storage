0328f774353b7da4462207eb32a2eeba69aaa45d
wjt <wjt19951215@gmail.com>
2026-08-22 14:11:40 -0700

    fix(test): 回退 conftest.py 中 Emulator 默认值为 192.168.0.165 共享地址

--- firebase/infrastructure/functions-py/tests/conftest.py
+++ firebase/infrastructure/functions-py/tests/conftest.py
@@ -3,8 +3,8 @@ import os
 
 import pytest
 
-EMULATOR_HOST = os.environ.get("XUAN_EMULATOR_HOST", "127.0.0.1:8080")
-AUTH_EMULATOR_HOST = os.environ.get("FIREBASE_AUTH_EMULATOR_HOST", "127.0.0.1:9099")
+EMULATOR_HOST = os.environ.get("XUAN_EMULATOR_HOST", "192.168.0.165:8080")
+AUTH_EMULATOR_HOST = os.environ.get("FIREBASE_AUTH_EMULATOR_HOST", "192.168.0.165:9099")
 
 
 @pytest.fixture(scope="session", autouse=True)
